96d7c527adab2f73310adaad4e6abf76e19820b7
Brandon Manzo <brandong.manzog@icloud.com>
2023-07-24 15:14:23 -0600

    finished fizz buzz

--- ejercicios/fizzbuzz.js
+++ ejercicios/fizzbuzz.js
@@ -6,3 +6,24 @@
 
 // [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz"]
 // Limite de numeros a revisar: 20
+
+function fizzBuzz(limit) {
+  const newArray = [];
+
+  for (let index = 1; index <= limit; index++) {
+    let fizz = index % 3 === 0;
+    let buzz = index % 5 === 0;
+    if (fizz && buzz) {
+      newArray.push("fizzBuzz");
+    } else if (fizz) {
+      newArray.push("fizz");
+    } else if (buzz) {
+      newArray.push("buzz");
+    } else {
+      newArray.push(index);
+    }
+  }
+  return newArray;
+}
+
+console.log(fizzBuzz(20));
