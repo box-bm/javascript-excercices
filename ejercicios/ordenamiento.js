869b85a8d395eb12ab46013d0671b7a71c944ce2
Brandon Manzo <brandong.manzog@icloud.com>
2023-07-24 14:43:45 -0600

    finished sort array

--- ejercicios/ordenamiento.js
+++ ejercicios/ordenamiento.js
@@ -2,16 +2,19 @@ const numberArray = [5, 7, 8, 1, 2, 5, 8, 4];
 // Indice           [0, 1, 2, 3, 4, 5, 6, 7, 8];
 // Numero           [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const listaOrdenada = [];
-
-for (const elemento of numberArray) {
+function sortArray(arr) {
+  let sortedArr = [arr[0]];
+  for (let i = 1; i < arr.length; i++) {
+    let j = 0;
+    while (j < sortedArr.length && arr[i] > sortedArr[j]) {
+      j++;
+    }
+    sortedArr.splice(j, 0, arr[i]);
+  }
+  return sortedArr;
 }
-// spread operator
-Math.max(...[5, 7, 8]); // 8
-Math.max(5, 7, 8); // 8
 
-Math.min(...[5, 7, 8]); // 5
-Math.min(5, 7, 8); // 5
+let listaOrdenada = sortArray(numberArray);
 
 // Resultado [1, 2, 4, 5, 5, 7, 8, 8]
 console.log(listaOrdenada);
