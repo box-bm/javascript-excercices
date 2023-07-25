6aeef2a7f5a6abf02f30653d0bf2da1b34b31672
Brandon Manzo <brandong.manzog@icloud.com>
2023-07-24 18:10:44 -0600

    added fizz buzz excercise

---
+++ ejercicios/fizzbuzz.js
@@ -0,0 +1,8 @@
+// fizz buzz
+// 1. Arreglo nuevo
+// 2. divisible en 3      -> "fizz"
+// 3. divisible en 5      -> "buzz"
+// 4. divisible en 3 y 5  -> "fizzbuzz"
+
+// [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz"]
+// Limite de numeros a revisar: 20
